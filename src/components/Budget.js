6143dbaeaa0be352cb2aa4ee4e697c68ccf119a1
Codemason43 <145558386+Israel-Arendse@users.noreply.github.com>
2024-05-19 14:55:55 +0400

    Update Budget.js
    Added a better code

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -6,12 +6,19 @@ const Budget = () => {
     const [newBudget, setNewBudget] = useState(budget);
     const handleBudgetChange = (event) => {
         setNewBudget(event.target.value);
-    }
+    };
+
     return (
-<div className='alert alert-secondary'>
-<span>Budget: £{budget}</span>
-<input type="number" step="10" value={newBudget} onChange={handleBudgetChange}></input>
-</div>
+        <div className='alert alert-secondary d-flex align-items-center'>
+            <div className='me-2'>Budget: £</div>
+            <input
+                type="number"
+                step="10"
+                value={newBudget}
+                onChange={handleBudgetChange}
+            />
+        </div>
     );
 };
+
 export default Budget;
