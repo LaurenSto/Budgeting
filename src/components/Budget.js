2b0f9d39067decee25a16718aaafe78c1b30f39b
Israel-Arendse <israelarendsefortnite@gmail.com>
2024-05-21 08:02:48 -0400

    modified Budget component

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,25 +1,26 @@
 import React, { useContext, useState } from 'react';
 import { AppContext } from '../context/AppContext';
 
+
 const Budget = () => {
     const { budget } = useContext(AppContext);
     const [newBudget, setNewBudget] = useState(budget);
     const [error, setError] = useState('');
-   
-
     
     const handleBudgetChange = (event) => {
         const inputValue = parseFloat(event.target.value);
-        
+    
         if (!isNaN(inputValue) && inputValue <= 20000) {
             setNewBudget(inputValue);
             setError(''); // Clear any previous error
+        } else if (inputValue < calculateTotalExpenses(expenses)) {
+            alert("Budget cannot be less than what is spent so far");
         } else {
             // Show an alert here (you can customize the styling)
             alert('Budget must be less than or equal to 20,000');
         }
-
     };
+    
 
 
 
