8ab3547d4872eadc76c1edc7424c0a2b4f4dbca1
BrunoMSPais <bpais@outlook.pt>
2024-05-21 17:43:07 -0400

    Finished hands-on lab: Budget Allocation App

--- src/App.js
+++ src/App.js
@@ -6,6 +6,10 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Budget from './components/Budget';
 
 // Add code to import the other components here under
+import Remaining from './components/Remaining';
+import ExpenseTotal from './components/ExpenseTotal';
+import ExpenseList from './components/ExpenseList';
+import AllocationForm from './components/AllocationForm';
 
 
 import { AppProvider } from './context/AppContext';
@@ -18,27 +22,48 @@ const App = () => {
                         {
                             /* Add Budget component here */
                         }        
+                        <div className='col-sm'>
+                            <Budget />
+                        </div>
 
                         {
                             /* Add Remaining component here*/
                         }        
+                        <div className='col-sm'>
+                            <Remaining />
+                        </div>
 
                         {
                             /* Add ExpenseTotal component here */
                         }        
+                        <div className='col-sm'>
+                            <ExpenseTotal />
+                        </div>
                        
-                        {
-                            /* Add ExpenseList component here */
-                        }         
 
-                        {
-                            /* Add ExpenseItem component here */
-                        }        
+                </div>
+                {
+                    /* Add ExpenseList component here */
+                }         
+                <h3 className='mt-3'>Allocation</h3>
+                <div className='row '>
+                    <div className='col-sm'>
+                        <ExpenseList />
+                    </div>
+                </div>
 
-                        {
-                            /* Add AllocationForm component here under */
-                        }        
+                {
+                    /* Add ExpenseItem component here */
+                }        
 
+                {
+                    /* Add AllocationForm component here under */
+                }        
+                <h3 className='mt-3'>Change allocation</h3>
+                <div className='row mt-3'>
+                    <div className='col-sm'>
+                        <AllocationForm/>
+                    </div>
                 </div>
             </div>
         </AppProvider>
--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,1 +1,17 @@
+import React, { useContext } from 'react';
+import { AppContext } from '../context/AppContext';
 
+const Budget = () => {
+    const { budget, dispatch } = useContext(AppContext);
+    // Removed local state management because Redux is managing the state
+    const handleBudgetChange = (event) => {
+        dispatch({ type: 'SET_BUDGET', payload: parseInt(event.target.value) })
+    }
+    return (
+        <div className='alert alert-secondary'>
+            <span>Budget: £</span>
+            <input type="number" step="10" value={budget} onChange={handleBudgetChange} style={{width: 'fit-content'}}></input>
+        </div>
+    );
+};
+export default Budget;
