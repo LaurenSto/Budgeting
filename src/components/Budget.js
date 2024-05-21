6f157bb4b6f00b445f5958e620c3d3d3cfae2fc4
Israel-Arendse <israelarendsefortnite@gmail.com>
2024-05-21 02:56:10 -0400

    Add minus buttons

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,17 +1,16 @@
 import React, { useContext, useState } from 'react';
 import { AppContext } from '../context/AppContext';
 
-
-
 const Budget = () => {
     const { budget } = useContext(AppContext);
     const [newBudget, setNewBudget] = useState(budget);
     const [error, setError] = useState('');
+   
 
     
     const handleBudgetChange = (event) => {
         const inputValue = parseFloat(event.target.value);
-
+        
         if (!isNaN(inputValue) && inputValue <= 20000) {
             setNewBudget(inputValue);
             setError(''); // Clear any previous error
