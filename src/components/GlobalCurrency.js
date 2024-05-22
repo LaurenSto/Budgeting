f4974f05daf3058a4e4aa139be149884f903e905
Codemason43 <145558386+Israel-Arendse@users.noreply.github.com>
2024-05-22 19:40:31 +0400

    Update GlobalCurrency.js
    Fixed GlobalCurrency.js component to display the currency prefixes properly

--- src/components/GlobalCurrency.js
+++ src/components/GlobalCurrency.js
@@ -1,26 +1,27 @@
-import React from 'react';
-import './GlobalCurrencyDropdown.css'
+// GlobalCurrency.js
+import React, { useContext } from 'react';
+import { AppContext } from '../context/AppContext';
 
-const GlobalCurrency = ({ onCurrencyChange }) => {
+const GlobalCurrency = () => {
+    const { dispatch } = useContext(AppContext);
+
     const handleCurrencyChange = (event) => {
         const selectedCurrency = event.target.value;
-        onCurrencyChange(selectedCurrency);
-        // Change currency prefix
+        dispatch({ type: 'CHG_CURRENCY', payload: selectedCurrency });
     };
 
     return (
         <div className='alert alert-secondary d-flex align-items-center'>
-           <div className='me-2'></div>
+            <div className='me-2'></div>
             <label htmlFor="currency-select">Currency: </label>
             <select
                 id="currency-select"
                 onChange={handleCurrencyChange}
                 className='currency-select'
-                >
-            
-                <option value="$">(£ Pound)</option>
+            >
+                <option value="£">(£ Pound)</option>
                 <option value="€">(€ Euro)</option>
-                <option value="£">($ Dollar)</option>
+                <option value="$">($ Dollar)</option>
                 <option value="₹">(₹ Rupee)</option>
                 {/* Add more currency options as needed */}
             </select>
