ac75a223a4c86d667cbc49fa714d5de152bfb49e
Malshi623 <mhsm.dulakshi@gmail.com>
2024-06-11 04:12:59 -0400

    Update Ex[enseList.js

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Budget from './components/Budget';
 // Add code to import the other components here under
 import Remaining from './components/Remaining';
 import ExpenseTotal from './components/ExpenseTotal';
+import ExpenseList from './components/ExpenseList';
 
 import { AppProvider } from './context/AppContext';
 const App = () => {
@@ -38,7 +39,10 @@ const App = () => {
                         }        
                        
                         {
-                            /* Add ExpenseList component here */
+                            //ExpenseList component
+                            // <div className='col-sm'>
+                            //     <ExpenseList />
+                            // </div>
                         }         
 
                         {
